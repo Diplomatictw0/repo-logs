2d65c618cf44bbea64aca710c881f8e4c8ba2d9b
Diplomatictw0 <j.cuervoo@uniandes.edu.co>
2024-10-05 17:14:35 -0500

    todo2

---
+++ django_app/logs_project/settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'logs_db',
+        'USER': 'logs_user',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
